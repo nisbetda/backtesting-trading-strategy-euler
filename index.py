124084b71e72c5f108421cb92de51a1b4578d6dd
nerfsunny <nerfsunny@gmail.com>
2021-08-05 16:49:06 -0400

    Changed code so that it creates dataframe from local file that is created when the data is pulled from website. Also, created dataframes for each year.

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 #import plotly.express as px
 #import plotly
-from datetime import date
+from datetime import date, datetime
 #import datapackage
 import csv
 import requests
@@ -53,31 +53,36 @@ import matplotlib.pyplot as plt
 #Use CoinCap API to request historical data
 url = 'http://api.coincap.io/v2/assets/bitcoin/history?interval=d1'#&start=1592585794000&end=1613753794000' #can we use a variable for the unix time 1592585794000 ? 1613753794000
 
-payload = {}
-headers = {}
+#payload = {}
+#headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+#response = requests.request("GET", url, headers=headers, data=payload)
 
-json_data = json.loads(response.text.encode('utf8'))
-bitcoin_data = json_data["data"]
+#json_data = json.loads(response.text.encode('utf8'))
+#bitcoin_data = json_data["data"]
 
 #2
 # create pandas dataframe
-df = pd.DataFrame(bitcoin_data)
-df.to_csv('bitcoin-data', index=False)
+#df = pd.DataFrame(bitcoin_data)
+#df.to_csv('bitcoin-data', index=False)
+
+df = pd.read_csv('bitcoin-data')
+
+print(df['date'])
+
 #show sample of data
-print(df.sample)
+#print(df.sample)
 #show data types
-print(df.dtypes)
+#print(df.dtypes)
 
 #create new data frame, from August 2019 to July 2021
-df = pd.DataFrame(bitcoin_data, columns=['time', 'priceUSD'])
+#df = pd.DataFrame(bitcoin_data, columns=['time', 'priceUSD'])
 #convert priceUSD from type object to float
-df['priceUSD'] = pd.to_numeric(df['priceUSD'], errors='coerce').fillna(0, downcast='infer')
+#df['priceUSD'] = pd.to_numeric(df['priceUSD'], errors='coerce').fillna(0, downcast='infer')
 #show data types
-print(df.dtypes)
+#print(df.dtypes)
 
-print(df.info)
+#print(df.info)
 
 
 #3. 
@@ -91,3 +96,7 @@ print(df.info)
 
 #https://docs.coincap.io/
 #https://www.youtube.com/watch?v=EoW4XFdh7gc
+
+df2019 = df[df['date'].str.contains('2019')]
+df2020 = df[df['date'].str.contains('2020')]
+df2021 = df[df['date'].str.contains('2021')]
